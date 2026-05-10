b732588698482971b2d264fec2890f206eb76129
Talbia khalid <talbiakhalid0@gmail.com>
2026-05-10 14:29:51 +0500

    Configured ApplicationDbContext and SQL connection string.

--- Lost And Found Portal/Components/Data/ApplicationDbContext.cs
+++ Lost And Found Portal/Components/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Lost_And_Found_Portal.Components.Data
+﻿using Lost_And_Found_Portal.Components.	Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Lost_And_Found_Portal.Components.Data
 {
-    public class ApplicationDbContext
-    {
-    }
+    public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options) { }
+
+		public DbSet<Item> Items { get; set; }
+	}
 }
