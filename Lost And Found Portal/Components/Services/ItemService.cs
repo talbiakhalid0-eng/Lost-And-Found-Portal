46db90ddf7034dc31e215609fbf58687bce62cfb
Talbia khalid <talbiakhalid0@gmail.com>
2026-05-10 17:16:27 +0500

    Added the core of the UI

--- Lost And Found Portal/Components/Services/ItemService.cs
+++ Lost And Found Portal/Components/Services/ItemService.cs
@@ -1,6 +1,38 @@
-﻿namespace Lost_And_Found_Portal.Components.Services
+﻿using Lost_And_Found_Portal.Components.Data;
+using Lost_And_Found_Portal.Components.Models;
+using Lost_And_Found_Portal.Components.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lost_And_Found_Portal.Services
 {
-    public class ItemService
-    {
-    }
+	public class ItemService : IItemService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ItemService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// Feature: Create/Post a New Item
+		public async Task<bool> CreateItemAsync(Item item)
+		{
+			try
+			{
+				_context.Items.Add(item);
+				var result = await _context.SaveChangesAsync();
+				return result > 0;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		// Feature: Get All Items for the Dashboard
+		public async Task<List<Item>> GetAllItemsAsync()
+		{
+			return await _context.Items.OrderByDescending(x => x.Id).ToListAsync();
+		}
+	}
 }
