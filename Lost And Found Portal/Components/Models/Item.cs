ad2706aab30c933b25efe074da224d7a03bf997c
Talbia khalid <talbiakhalid0@gmail.com>
2026-05-10 14:17:17 +0500

    Inherited Item class with BaseEntity

--- Lost And Found Portal/Components/Models/Item.cs
+++ Lost And Found Portal/Components/Models/Item.cs
@@ -1,6 +1,15 @@
 ﻿namespace Lost_And_Found_Portal.Components.Models
 {
-    public class Item
-    {
-    }
+    public class Item : BaseEntity
+	{
+		public string Title { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string Category { get; set; } = "General";
+		public string Location { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = "/images/placeholder.png";
+		public bool IsLost { get; set; } // True = Lost, False = Found
+		public string Status { get; set; } = "Unclaimed";
+		public string ContactInfo { get; set; } = string.Empty;
+		public string UserId { get; set; } = string.Empty;
+	}
 }
