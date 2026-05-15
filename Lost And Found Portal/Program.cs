0e40a026a8f1cbc344b72edba7c6bcdc44c03926
Talbia khalid <talbiakhalid0@gmail.com>
2026-05-15 18:19:06 +0500

    handled database schema sync for DatePosted

--- Lost And Found Portal/Program.cs
+++ Lost And Found Portal/Program.cs
@@ -13,6 +13,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddScoped<IItemService, ItemService>();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+	var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+	context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
